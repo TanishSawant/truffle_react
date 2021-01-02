1344eaa39c7f2ecc9bee9bc93843c39a729c809a
TANISH SAWANT <tanishsawant@rediffmail.com>
2021-01-02 20:39:22 +0530

    Interacting with contract in react.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+//import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import walletContract from "./contracts/wallet.json"
 import getWeb3 from "./getWeb3";
 import "./App.css";
+import Web3 from "web3";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { web3: null, accounts: null, contract: null, balance: 0 , owner: ""};
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
@@ -16,9 +17,9 @@ class App extends Component {
 
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const deployedNetwork = walletContract.networks[networkId];
       const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
+        walletContract.abi,
         deployedNetwork && deployedNetwork.address,
       );
 
@@ -35,16 +36,21 @@ class App extends Component {
   };
 
   runExample = async () => {
-    const { accounts, contract } = this.state;
-
+    const { contract } = this.state;
+    const web3 = new Web3()
     // Stores a given value, 5 by default.
-    await contract.methods.set(15).send({ from: accounts[0] });
+    //await contract.methods.set(15).send({ from: accounts[0] });
 
     // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
+    const response = await contract.methods.getBal().call();
+    const _owner = await contract.methods.owner().call();
+    console.log('Sending money....')
+    const sendmoney_response = await contract.methods.sendMoney().call({value : web3.utils.toWei(web3.utils.toBN(12), 'ether')});
+    const withdraval_receipt = await contract.methods.withdraw(web3.utils.toWei("1", 'ether')).call()
+    console.log('withdraw')
+    console.log(withdraval_receipt)
     // Update state with the result.
-    this.setState({ storageValue: response });
+    this.setState({ balance : response , owner : _owner});
   };
 
   render() {
@@ -54,7 +60,9 @@ class App extends Component {
     return (
       <div className="App">
         <h2>Smart Contract Example</h2>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <h3>Owner : {this.state.owner}</h3>
+
+        <div>Your balance is is: {this.state.balance}</div>
       </div>
     );
   }
